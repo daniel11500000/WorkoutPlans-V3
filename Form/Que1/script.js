f4150af26acf67b9d8fe819cf9b49f16c6566228
daniel11500000 <118695412+daniel11500000@users.noreply.github.com>
2023-05-29 19:39:48 +0200

    Add files via upload

---
+++ Form/Que1/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// Selectors
+
+const logo = document.querySelector(".lift");
+const filling = document.querySelector(".filling");
+const awnser = document.querySelectorAll(".awnser");
+const pro = document.querySelector(".pro");
+const backBtn = document.querySelector(".backBtn");
+
+// Events
+
+logo.addEventListener("click", function (e) {
+  window.location.href = "/mainPage/mainPage.html";
+});
+
+// Progress Bar
+const filePath = window.location.pathname;
+const fileName = filePath.slice(6, 10);
+const fileNum = fileName.slice(3, 4);
+
+// Progress Bar
+const total = 100 / 6;
+filling.style.width = total * (fileNum - 1) + "%";
+
+pro.textContent = fileNum + "/6";
+
+// Back Button
+if (fileNum == 1) {
+  backBtn.style.opacity = "0%";
+  backBtn.classList.remove("pointer");
+} else {
+  backBtn.style.opacity = "100%";
+  backBtn.classList.add("pointer");
+  backBtn.addEventListener("click", (e) => {
+    const prevPageName = "Que" + (Number(fileNum) - 1);
+    window.location.href = "../" + prevPageName + "/index.html";
+  });
+}
+
+// Question itself
+awnser.forEach(function (el) {
+  el.addEventListener("click", (e) => {
+    if (el.classList.contains("awnser-1")) localStorage.setItem(fileName, 1);
+    if (el.classList.contains("awnser-2")) localStorage.setItem(fileName, 2);
+    if (el.classList.contains("awnser-3")) localStorage.setItem(fileName, 3);
+    if (el.classList.contains("awnser-4")) localStorage.setItem(fileName, 4);
+    // Next window
+    const nextPage = Number(fileNum) + 1;
+    console.log(nextPage);
+    const nextPageName = "Que" + nextPage;
+    console.log(nextPageName);
+    window.location.href = "../" + nextPageName + "/index.html";
+  });
+});
